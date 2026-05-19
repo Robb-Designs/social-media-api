2d41eecb8d525c3a54ce6206390a1c9f4b44585c
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-19 02:03:11 -0400

    add port for listening

--- server.js
+++ server.js
@@ -18,3 +18,6 @@ const PORT = process.env.PORT;
 
 
 //PORT----------------------------------------------------------------------------------------
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
